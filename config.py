65c35d7493d44dc8cca0adcdb15811c1246f1ec6
Todd Birchard <toddbirchard@gmail.com>
2020-05-02 22:12:49 -0400

    Replace setup.py with pyproject.toml.

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 """App configuration."""
 from os import environ
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
-    """Set Flask configuration vars from .env file."""
+    """Set Flask configuration variables from .env file."""
 
     # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_APP = 'wsgi.py'
 
     # Flask-Assets
     LESS_BIN = environ.get('LESS_BIN')
